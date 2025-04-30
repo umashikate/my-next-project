21ddf0d6d1db3877507c6deba08cebd7dc392052
umashikate <yuno.a725@icloud.com>
2025-04-30 15:05:01 +0900

    chapter4

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,38 @@
-import styles from "./page.module.css";
-import Image from "next/image";
+import styles from './page.module.css';
+import Image from 'next/image';
 
-import NewsList from "@/app/_components/NewsList";
-import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
+import NewsList from '@/app/_components/NewsList';
+import ButtonLink from '@/app/_components/ButtonLink';
+import { News } from '@/app/_libs/microcms';
 
 const data: { contents: News[] } = {
   contents: [
     {
-      id: "1",
-      title: "渋谷にオフィスを移転しました",
+      id: '1',
+      title: '渋谷にオフィスを移転しました',
       category: {
-        name: "更新情報",
+        name: '更新情報',
       },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
+      publishedAt: '2023/05/19',
+      createdAt: '2023/05/19',
     },
     {
-      id: "2",
-      title: "当社CEOが業界リーダーTOP30に選出されました",
+      id: '2',
+      title: '当社CEOが業界リーダーTOP30に選出されました',
       category: {
-        name: "更新情報",
+        name: '更新情報',
       },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
+      publishedAt: '2023/05/19',
+      createdAt: '2023/05/19',
     },
     {
-      id: "3",
-      title: "テストの記事です",
+      id: '3',
+      title: 'テストの記事です',
       category: {
-        name: "更新情報",
+        name: '更新情報',
       },
-      publishedAt: "2023/04/19",
-      createdAt: "2023/04/19",
+      publishedAt: '2023/04/19',
+      createdAt: '2023/04/19',
     },
   ],
 };
